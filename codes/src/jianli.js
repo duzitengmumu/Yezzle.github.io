6b988e2cbc5d7f1069237fa7d3ea0497f0a8df9d
yezzle <625427555@qq.com>
2020-03-22 22:37:03 +0800

    update

--- codes/src/jianli.js
+++ codes/src/jianli.js
@@ -2,40 +2,49 @@ export default `
 # 求职意向: 前端开发工程师
 ---
 ## 基本信息
-  + 姓名: Yezzle    毕业院校：中国地质大学
-
-  + 年龄: 26      联系电话：13409776570
+  + 姓名: Yezzle &nbsp;&nbsp;&nbsp;   毕业院校：中国地质大学
+  + 年龄: 26  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;    联系电话：13409776570
   + 专业：测绘工程
 
 ## 技能
 
-0. 熟练掌握\`H5、CSS3、ES6\`
-0. 熟练使用不同主流的UI框架：\`ElementUI、Antd\`
-0. 掌握前端主流开发框架: React全家桶、Vue全家桶
-0. 熟悉后端语言Java、 node.js
-0. 熟练使用前端构建工具webpack、gulp
-0. 熟悉原生安卓开发、electron桌面应用开发
-0. 熟悉linux操作系统、熟练使用docker
+0. 熟练掌握**H5、CSS3、ES6**等基本知识
+0. 熟练使用不同主流的UI框架：**ElementUI、Antd**
+0. 掌握前端主流开发框架: **React**全家桶、**Vue**全家桶，掌握其原理
+0. 了解微信**小程序**开发，有自己的微信小程序
+0. 熟悉后端语言**Java、 node.js**，熟悉数据库操作，有**express**实践经验
+0. 熟练使用前端构建工具**webpack、gulp**，做过打包优化
+0. 熟悉原生安卓开发、**electron**桌面应用开发
+0. 熟悉**linux**操作系统、熟练使用**docker**，有自己的网站服务器
 
 ## 工作经历
 
 |时间|岗位|简介|
 |---|---|---|
-|2017.3-2017.6 |Java实习生 |负责公司项目业务开发，编写业务代码，打包部署|
+|2017.3-2017.6&nbsp;&nbsp; |中软国际 Java实习生&nbsp;&nbsp; |负责公司后端业务开发，编写业务代码，打包部署|
+|2017.6-2018.8&nbsp;&nbsp; |中软国际 前端工程师&nbsp;&nbsp; |负责公司前端业务开发，维护项目代码 |
+|2018.3-至今 &nbsp;&nbsp;  |边玩边学 前端工程师&nbsp;&nbsp; |负责公司前端业务开发和业务方案研究设计，前端组长|
+
+## 自我评价
+
+0. 喜欢钻研前端技术，经常翻阅前端主流社区学习(github,掘金,简书,b站)
+0. 最近开始写[简书博客(https://www.jianshu.com/u/4d1af407cc51)](https://www.jianshu.com/u/4d1af407cc51)，[github主页(https://yezzle.github.io)](https://yezzle.github.io)
+0. 热爱前端技术，思维活跃，学习能力强，有一定的抗压能力
+0. 性格随和，诚恳稳重，能很快适应新环境
 `
 
-export const cssStyle = `
+export const cssStyles = `
 .box{
+  transition: linear all 0.5s;
   width: 860px;
   position: relative;
   margin: auto;
-  /*避免闪烁分开写*/
-  padding-top: 20px;
-  padding-left: 10px;
+  padding: 20px 10px;
   left: 0;
   right: 0;
   background-color: beige;
   min-height: 100vh;
+  border-radius: 2px;
 }
 
 h1,h2,h3,h4,h5{
--- codes/src/pages/index.js
+++ codes/src/pages/index.js
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import styles from './index.css'
 import marked from "marked";
 import { tickRender, renderMarked, createStyleElement, renderCss } from "@/utils/funcs"
-import data, { cssStyle } from '@/jianli.js'
+import data, { cssStyles } from '@/jianli.js'
 
 export default function(){
+
+  const ref = useRef(null)
   
-  function setRef(elm){
-    if(!elm) return;
-    tickRender(data, renderMarked, 50 ,elm ).then(()=>{
-      // elm.innerHTML = marked(data)
-      const styleElement = createStyleElement()
-      tickRender(cssStyle, renderCss, 50, styleElement)
+  useEffect(() => {
+    if(!ref) return;
+    const styleElement = createStyleElement()
+    // ref.current.innerHTML = marked(data)
+    // styleElement.innerHTML = cssStyles
+
+    tickRender(data, renderMarked, 20 , ref.current ).then(()=>{
+      console.log('渲染完成')
     })
-  }
+    tickRender(cssStyles, renderCss, 80, styleElement)
+    
+  }, [])
 
   return (
-    // <div className={styles.box} ref={setRef}>
-    <div className='box' ref={setRef}>
+    // <div className={styles.box} ref={setRef} >
+    <div className='box' ref={ref}> 
     </div>
-  );
+  )
 }
